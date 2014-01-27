1a9ae63048db5c9b725ffdf2681b3dbfe041325f
Roger Adams <adamsrog04@gmail.com>
2014-01-26 23:06:47 -0800

    completed chapter 10 - comments

--- client/views/includes/errors.js
+++ client/views/includes/errors.js
@@ -3,3 +3,13 @@ Template.errors.helpers({
 		return Errors.find();
 	}
 });
+
+// .rendered runs directly after page finished (1ms delay)
+Template.error.rendered = function() {
+	var error = this.data;
+	Meteor.defer(function() {
+		Errors.update(error._id, {
+			$set: {seen: true}
+		});
+	});
+};
---
+++ collections/comments.js
@@ -0,0 +1,30 @@
+Comments = new Meteor.Collection('comments');
+
+Meteor.methods({
+	comment: function(commentAttributes) {
+		var user = Meteor.user();
+		var post = Posts.findOne(commentAttributes.postId);
+
+		// error checking
+		if (!user)
+			throw new Meteor.Error(401, "You need to log in to make comments.");
+		if (!commentAttributes.body)
+			throw new Meteor.Error(422, "No content in comment.");
+		if (!post)
+			throw new Meteor.Error(422, "You must comment on a post.");
+
+		// create the comment object that'll be inserted
+		comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+
+		// increment the comments count (denormalization)
+		Posts.update(comment.postId, {
+			$inc: {commentsCount: 1}
+		});
+
+		return Comments.insert(comment);
+	}
+});
